eaeca82b132b938555b0c83a4e93e53667de1fc9
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-08-02 06:56:16 +0530

    imports in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,16 +7,27 @@ import Server from "./server"
 
 const app: Express = express()
 
-// routers imports
-
 // middlewares
 app.use(cors())
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // start server
 Server.startServer(app)
+
+/**
+ *  ======================================================================
+ *  ======================================================================
+ *  ======================================================================
+ */
+
+// imports
+import jwt from "jsonwebtoken"
+
+// models
+import { User } from "./models/user"
+import { Message } from "./models/message"
 
 // routes
 app.get("/", (req: Request, res: Response) => {
